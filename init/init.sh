3fef46c69d2bed9506259d0ae51153b4cc834819
Ryan Frantz <ryanleefrantz@gmail.com>
2012-11-29 13:12:14 -0500

    Starting work on updating init.sh to use parseYaml.py.

--- init/init.sh
+++ init/init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# TODO: lots...
+# TODO:
+# 1. account for the fact that a user may decide to change defaults
+# 2. set the appropriate permissions for Apache to own/read the .db file
 
 # create a home for our db
 mkdir -p /root/concierge/sqlite
 # remove a pre-existing db
-rm -f /root/concierge/sqlite/appStatus.db
+#rm -f /root/concierge/sqlite/appStatus.db
+rm -f /root/concierge/sqlite/concierge.db
 # seed our db
-sqlite3 -init /root/concierge/init/init.sql /root/concierge/sqlite/appStatus.db
+/usr/bin/python /root/concierge/test/parseYaml.py
